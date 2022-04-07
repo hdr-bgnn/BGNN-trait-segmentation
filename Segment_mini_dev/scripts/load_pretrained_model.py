3543aa112f8b2138b96ca475c0f7e9119c0d38f2
thibaulttabarin <thibault.tabarin@gmail.com>
2022-04-07 12:02:04 -0600

    add load_trained_model.py for container

---
+++ Segment_mini_dev/scripts/load_pretrained_model.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import numpy as np
+from PIL import Image
+import torch
+from torchvision import transforms
+import helper_mini as sh
+import warnings
+
+def main(): 
+
+     model, preprocessing_fn, CLASSES = sh.load_pretrained_model()
+
+if __name__ == '__main__':
+
+  main()
